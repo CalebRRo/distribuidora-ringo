6d40df4b3c6a2967ce154cd62bde94bcda4a5d8c
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-05-14 19:34:26 -0300

    rm carrito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import CARRITO from "./carrito";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CARRITO />
         <p>DISTRIBUIDORA RINGO </p>
       </header>
       <body />
